3586298c24b5e15bbd6a5358ca8496e2560031b9
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2023-10-28 15:49:37 -0300

    :feat: operacoes implementadas com sucesso

--- Program.cs
+++ Program.cs
@@ -5,8 +5,15 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+var env = builder.Environment; 
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(env.ContentRootPath)  
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+    .Build();
+
+builder.Services.AddDbContext<OrganizadorContext>(Options =>
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
